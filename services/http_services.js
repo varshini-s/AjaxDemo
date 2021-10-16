83d932e1fac7ad09de33d8734562f23ea542b48e
varshini <dreamvarshini@gmail.com>
2021-10-16 21:22:05 +0530

    ADDED: function to use promise to send and reciever data

---
+++ services/http_services.js
@@ -0,0 +1,46 @@
+
+function makePromiseCall(methodType,url,async=true,data=null)
+{
+    return new Promise(function(resolve,request)
+    {
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function(){
+            console.log(methodType+" state changed called Ready state: "+xhr.readyState+" Status: "+xhr.status)
+            
+            if(xhr.status.toString().match('^[2][0-9]{2}$'))
+            {
+                resolve(xhr.responseText)
+            }
+            else if(xhr.status.toString().match('^[4,5][0-9]{2}$'))
+            {
+                reject({
+                    status:xhr.status,
+                    statusText:xhr.statusText
+                });
+                console.log("XHR failed");
+            }
+ 
+        }
+        xhr.onerror=function(){
+            reject({
+                status:xhr.status,
+                statusText:xhr.statusText
+            });
+        }
+        xhr.open(methodType,url,async);
+
+        if(data)
+        {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type","application/json");
+            xhr.send(JSON.stringify(data));
+        }
+        else
+        {
+            xhr.send();
+        }
+        console.log(methodType+" Request sent to server ")
+    
+    });
+}
+
